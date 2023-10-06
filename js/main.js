fb170125f469065175b26cf629cc49aa9bafaa0e
anastasiya1702 <voronovanastya988@gmail.com>
2023-10-06 18:14:14 +0300

    added header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
 $(function () {
 
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+  const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
+  // function getTimeRemaining(endtime) {
+  //   const total = Date.parse(endtime) - Date.parse(new Date());
+  //   const seconds = Math.floor((total / 1000) % 60);
+  //   const minutes = Math.floor((total / 1000 / 60) % 60);
+  //   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  //   const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  //   return {
+  //     total,
+  //     days,
+  //     hours,
+  //     minutes,
+  //     seconds
+  //   };
+  // }
+
+  // function initializeClock(id, endtime) {
+  //   const clock = document.querySelector('.promo__clock');
+  //   const daysSpan = clock.querySelector('.promo__days');
+  //   const hoursSpan = clock.querySelector('.promo__hours');
+  //   const minutesSpan = clock.querySelector('.promo__minutes');
+  //   const secondsSpan = clock.querySelector('.promo__seconds');
+
+  //   function updateClock() {
+  //     const t = getTimeRemaining(endtime);
+
+  //     daysSpan.innerHTML = t.days;
+  //     hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+  //     minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+  //     secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+  //     if (t.total <= 0) {
+  //       clearInterval(timeinterval);
+  //     }
+  //   }
+
+  //   updateClock();
+  //   const timeinterval = setInterval(updateClock, 1000);
+  // }
+
+  // const deadline = $('.promo__clock').attr('data-time');
+  // initializeClock('promo__clock', deadline);
+
+  // $('.footer-top__title').on('click', function () {
+  //   $(this).siblings().slideToggle();
+  //   $(this).toggleClass('footer__title-list--active');
+  // });
+
 });
